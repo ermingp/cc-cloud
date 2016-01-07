c4f173b7cecc10a36d45d148f687087e349123e0
Marc Gariepy <gariepy.marc@gmail.com>
2016-01-07 14:48:07 +0000

    adding network creation.

--- odc2016/project-manage.py
+++ odc2016/project-manage.py
@@ -6,10 +6,12 @@
 import os,sys
 from  keystoneclient.v2_0 import client as ks_client
 from novaclient import client as nova_client
+from neutronclient.neutron import client as neutron_client
 import keystoneclient.exceptions as ex
 
 import json
 import urllib2
+from random import randint
 
 # read env for needed vars
 try:
@@ -20,6 +22,8 @@ try:
   cloud_api_key=os.environ['CLOUD_API_KEY']
   cloud_api_host=os.environ['CLOUD_API_HOST']
   cloud=os.environ['CLOUD']
+  cloud_admin=os.environ['CLOUD_ADMIN']
+  cloud_admin_password=os.environ['CLOUD_ADMIN_PASSWORD']
 except KeyError, e:
   print "Please export the following key: %s, or source the openrc file" % e
   sys.exit(1)
@@ -27,7 +31,7 @@ except KeyError, e:
 keystone=ks_client.Client(username=username, password=password, \
                        tenant_name=tenant_name, auth_url=auth_url)
 
-creds={'api_key': password, 'auth_url': auth_url, 'username': username }
+creds={'api_key': cloud_admin_password, 'auth_url': auth_url, 'username': cloud_admin }
 
 #print type(keystone.tenants.list())
 
@@ -40,6 +44,7 @@ def create_project(name=None, description=None, mentor=None, configuration=None)
   description: (tenant['description'])
   mentor: tenant['odc_application']['mentor']['username']
   """
+  project_exist=False
   try:
     project=keystone.tenants.create(tenant_name=name, description=description, enabled=True)
   except ex.Conflict, e: 
@@ -47,19 +52,46 @@ def create_project(name=None, description=None, mentor=None, configuration=None)
     if project == None:
       print "Error, cannot create project and it doesn't exist"
       sys.exit(1)
+    else:
+      print "project already exist"
+      project_exist=True
   try:
     # error on duplicate, no error when the user doesn't exist.
     project.add_user(user=mentor,role=find_role_id())
     project.add_user(user='mgariepy',role=find_role_id())
+    project.add_user(user='cccs',role=find_role_id())
   except ex.Conflict, e:
     # already a member
     pass
  
   print project.list_users()
   print project.id
-  set_quota(project=project, config=configuration)  
+  #set_quota(project=project, config=configuration)  
+
+  #create the network, only if the project doesn't already exist. shoud add support to detect when the network already exist at some point.
+  if project_exist == False:
+    n_client=neutron_client.Client('2.0', auth_url=auth_url,username=username, \
+                 tenant_name=os.environ['OS_TENANT_NAME'],password=password)
+    network = { 'name': tenant_name.replace(' ','_')+'_network', 
+                'admin_state_up': True, 
+                'tenant_id':project.id }
+    network_info=n_client.create_network( { 'network': network })
   
-  
+    subnet =  { 'name': tenant_name.replace(' ','_')+'_subnet', 
+                'network_id': network_info['network']['id'],
+                'tenant_id': network_info['network']['tenant_id'],
+                'cidr': '192.168.%i.0/24' % (randint(0,254)),
+                'enable_dhcp': True,
+                'ip_version': 4 }
+    subnet_info=n_client.create_subnet( { 'subnet': subnet })
+    
+    router = { 'name'; tenant_name.replace(' ','_')+'_router',
+               'tenant_id' : network_info['network']['tenant_id'] }
+    router_info=n_client.create_router( 'router': router })
+    # this is specific to East-cloud. (network_id is set to the external network id)
+    n_client.add_gateway_router(router_info['router']['id'], {u'enable_snat': True,'network_id': u'f6a2af4a-f7c2-4d68-9c28-63714c931ec0'})
+    n_client.add_interface_router(router_info['router']['id'], {'subnet_id': subnet_info['subnet']['id']})
+
 def set_quota(project=None, config=None):
   """
   nova quota-update $tenant_id --instances $Q_instances --cores $Q_cores \
@@ -100,13 +132,16 @@ def find_role_id(name="Member"):
 def add_ssh_key():
   """
   This function will add the public key to a user.
+  this need to be run from the user account (cccs/cloud-adm or something else.)
   """
+  
   pass
 
 def build_instance():
   """
   This function will create the instance, associate the floating ip, and 
   configure security rules (ssh/rdesktop, + something else).
+  this need to be run from the user account (cccs/cloud-adm or something else.)
   """
   pass
 
